d697b615c4e064b1bfa930c8b3ff5254e3d11d39
Cyriaque Perier <cyriaque.perier@gmail.com>
2021-08-22 15:32:25 +0200

    Debug is now cleaner.

--- mach4/utils/text.py
+++ mach4/utils/text.py
@@ -13,12 +13,12 @@ class Colors:
     
     """
     
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    GRAY   = "\033[1;30m"
+    RED    = "\033[1;31m"
+    GREEN  = "\033[0;32m"
+    YELLOW = "\033[1;33m"
+    BLUE   = "\033[1;34m"
+    PURPLE = "\033[1;35m"
+    CYAN   = "\033[1;36m"
+    WHITE  = "\033[1;37m"
+    RESET = "\033[0m"
